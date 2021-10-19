c277af7ae2da2ffc31466f4b0948bfb77408fc01
lystun <okegbemiolatunde@gmail.com>
2021-10-19 02:15:03 +0100

    project description

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,8 +5,8 @@ export default {
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: '' },
-			{ name: 'format-detection', content: 'telephone=no' }
+			{ hid: 'description', name: 'description', content: 'Africa Blockchain Report 2021' },
+			{ name: 'format-detection', content: 'telephone=no' },
 		],
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
