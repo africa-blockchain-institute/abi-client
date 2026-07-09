424dd8579012f92edb9f504a37dec888bad46684
lystun <okegbemiolatunde@gmail.com>
2026-07-09 17:24:12 +0100

    fix: base url update

--- nuxt.config.js
+++ nuxt.config.js
@@ -89,9 +89,9 @@ export default {
 	
 	// Axios module configuration: https://go.nuxtjs.dev/config-axios
 	axios: {
-		baseURL: "http://127.0.0.1:8081/api/v1",
+		// baseURL: "http://127.0.0.1:8081/api/v1",
 		// baseURL: "https://africabi-api.herokuapp.com/api/v1",
-		// baseURL: "https://abi-api.onrender.com/api/v1",
+		baseURL: "https://abi-api.onrender.com/api/v1",
 	},
 
 	auth:{
